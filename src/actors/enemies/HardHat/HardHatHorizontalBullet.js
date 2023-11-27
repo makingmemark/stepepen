bdcb8c117da29c9d4b595637d0d5879828e5e709
Mark Lowe <mark@liquid.com.au>
2023-11-27 14:44:26 +1100

    black bullets

--- src/actors/enemies/HardHat/HardHatHorizontalBullet.js
+++ src/actors/enemies/HardHat/HardHatHorizontalBullet.js
@@ -5,11 +5,11 @@ import { LEFT } from "../../../constants.js";
 export class HardHatHorizontalBullet extends ex.Actor {
   constructor(x, y, direction) {
     super({
-      pos: new ex.Vector(x, y),
-      width: 8,
+      pos: new ex.Vector(x, y + 10),
+      width: 4,
       height: 4,
       scale: new ex.Vector(2, 2),
-      color: ex.Color.Yellow,
+      color: ex.Color.Black,
 
       collider: ex.Shape.Box(8, 4, ex.Vector.Zero),
       collisionType: ex.CollisionType.Passive,
